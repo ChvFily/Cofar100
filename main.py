299024dd7b9ba89f8e808a7f36b850b970ab9f44
ChvFily <1274152433@qq.com>
2021-01-06 09:04:29 +0800

    main-1

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ conv_layers = [ ## 5 unit conv + max pooling
 fc_layers = [
     layers.Dense(256,activation=tf.nn.relu),
     layers.Dense(128,activation=tf.nn.relu),
-    layers.Dense(100,activation=tf.nn.relu)
+    layers.Dense(100)
 ]
 
 ## 预处理数据集
@@ -56,10 +56,10 @@ db_train = tf.data.Dataset.from_tensor_slices((x,y))
 db_train = db_train.shuffle(10000).map(preprocess).batch(64)
 
 db_test = tf.data.Dataset.from_tensor_slices((x_test,y_test))
-db_test = db_test.map(preprocess)
+db_test = db_test.map(preprocess).batch(64)
 
 
-# print(x.shape,y.shape,x_test.shape,x_test.shape)
+print(x.shape,y.shape,x_test.shape,y_test.shape)
 
 
 def main():
@@ -70,38 +70,39 @@ def main():
     conv_net.build(input_shape=[None,32,32,3])
     fc_net.build(input_shape=[None,512])
     variables = conv_net.trainable_variables + fc_net.trainable_variables ## 结合参数
-    optimizer = optimizers.Adam(lr = 1e-4 ) ## 学习率
+    optimizer = optimizers.Adam(lr = 1e-3) ## 学习率
 
     # x = tf.random.normal([4,32,32,3]) ## 初始化参数
     # out = network(x)
     # print(out.shape)
 
-    for epochs in range(10):
+    for epochs in range(100):
         """
         train datasets
         """
-        for step , (x,y) in enumerate(db_train):
-            with tf.GradientTape() as tape:
-                ## 输出层
-                out = conv_net(x)  ## out [b,1,1,512] 
-                out = tf.reshape(out,[-1,512])  ## => [c,512] 顺应参数的集合
-                logits = fc_net(out) 
-                # logits = tf.nn.softmax(logits)
-                y_one_hot = tf.one_hot(y,depth=100)
+        # for step , (x,y) in enumerate(db_train):
+        #     with tf.GradientTape() as tape:
+        #         ## 输出层
+        #         out = conv_net(x)  ## out [b,1,1,512] 
+        #         out = tf.reshape(out,[-1,512])  ## => [c,512] 顺应参数的集合
+        #         logits = fc_net(out) 
+        #         # logits = tf.nn.softmax(logits)
+        #         y_one_hot = tf.one_hot(y,depth=100)
 
-                ## loss compution
-                loss = tf.losses.categorical_crossentropy(y_one_hot,logits,from_logits=True)
-                loss = tf.reduce_mean(loss)
-            ## 计算梯度
-            grades = tape.gradient(loss,variables)  ## 函数 与 需要优化的参数 
-            optimizer.apply_gradients(zip(grades,variables))
-            if step % 100 ==0 :
-                print(step,"loss:",float(loss))
+        #         ## loss compution
+        #         loss = tf.losses.categorical_crossentropy(y_one_hot,logits,from_logits=True)
+        #         loss = tf.reduce_mean(loss)
+        #     ## 计算梯度
+        #     grades = tape.gradient(loss,variables)  ## 函数 与 需要优化的参数 
+        #     optimizer.apply_gradients(zip(grades,variables))
+        #     if step % 100 ==0 :
+        #         print(step,"loss:",float(loss))
 
         """"
         test datasets
         """
-        for x,y in db_test:
+        toatal_correct,total_sum = 0 , 0
+        for x , y in db_test:
             out = conv_net(x)  ## out [b,1,1,512] 
             out = tf.reshape(out,[-1,512])  ## => [c,512] 顺应参数的集合
             logits = fc_net(out) 
@@ -109,12 +110,12 @@ def main():
             pred = tf.argmax(prob,axis=1) ## 得到可能行最大的下标
             pred = tf.cast(pred,dtype=tf.int32) 
             ## 计算正确的数量
-            correct = tf.cast(tf.equal(pred,y),dtype=int32)
+            correct = tf.cast(tf.equal(pred,y),dtype=tf.int32)
             correct = tf.reduce_sum(correct)
             toatal_correct += correct
             total_sum += x.shape[0]
             acc = toatal_correct / total_sum
-        print(step,"acc:",acc)
-        
+        print(epochs,"acc:",acc)
+
 if __name__ == "__main__":
     main()
